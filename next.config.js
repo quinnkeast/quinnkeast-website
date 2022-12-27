48da17a237c854a879ae798036fc4730ebff2913
quinnkeast <hey@quinnkeast.com>
2022-12-27 11:00:01 -0600

    Resolve config error

--- next.config.js
+++ next.config.js
@@ -2,9 +2,7 @@ module.exports = {
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
+        config.resolve.fallback.fs = false;
     }
 
     return config
