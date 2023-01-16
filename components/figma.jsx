b5a4edb2d982f1c33a4070762336a405e74a67bc
quinnkeast <hey@quinnkeast.com>
2023-01-16 17:01:28 +0100

    Progress on code monitoring case study

--- components/figma.jsx
+++ components/figma.jsx
@@ -1,18 +1,25 @@
-export default function Figma({ link, title }) {
+export default function Figma({ link, title, thumbnail }) {
   return (
     <a href={link} target="_blank">
-      <div className="flex flex-row items-center bg-white rounded-md p-4 my-4 shadow">
-        <img
-          src="/assets/figma-logo.svg"
-          width="28"
-          height="39"
-          className="mr-4"
-        />
+      <div className="flex flex-col bg-white rounded-md p-4 my-4 shadow">
+        {thumbnail && (
+          <div className="flex flex-row mb-3">
+            <img src={thumbnail} className="rounded-md" />
+          </div>
+        )}
+        <div className="flex flex-row items-center">
+          <img
+            src="/assets/figma-logo.svg"
+            width="28"
+            height="39"
+            className="mr-4"
+          />
 
-        <div className="leading-tight">
-          {title && <span className="font-medium mb-1">{title}</span>}
-          <br />
-          <span className="text-black-lighter text-sm">View Figma file</span>
+          <div className="leading-tight">
+            {title && <span className="font-medium mb-1">{title}</span>}
+            <br />
+            <span className="text-black-lighter text-sm">View Figma file</span>
+          </div>
         </div>
       </div>
     </a>
