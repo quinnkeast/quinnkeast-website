8b60a26435734de1e365ed492458c4ae440c44bf
quinnkeast <hey@quinnkeast.com>
2023-02-03 17:14:43 +0100

    Fix talk, add slide row component

---
+++ components/slide-row.jsx
@@ -0,0 +1,11 @@
+export default function SlideRow({ slide, children }) {
+  if (!slide || !children) return;
+  return (
+    <div class="grid md:grid-cols-5 md:gap-8">
+      <div class="col-span-3">{children}</div>
+      <div class="col-span-2 pt-3">
+        <img src={slide} class="border-4 border-white" />
+      </div>
+    </div>
+  );
+}
