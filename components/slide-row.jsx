dd6e644058eba437dd0701697126c29f6ef9ff2c
quinnkeast <hey@quinnkeast.com>
2023-02-06 10:50:30 +0100

    Implement slide row component as a temp fix

--- components/slide-row.jsx
+++ components/slide-row.jsx
@@ -1,10 +1,17 @@
+//import Image from "next/image";
+
 export default function SlideRow({ slide, children }) {
   if (!slide || !children) return;
   return (
     <div class="grid md:grid-cols-5 md:gap-8">
       <div class="col-span-3">{children}</div>
       <div class="col-span-2 pt-3">
-        <img src={slide} class="border-4 border-white" />
+        <img
+          src={slide}
+          className="border-4 border-white"
+          //width={400}
+          //height={400}
+        />
       </div>
     </div>
   );
