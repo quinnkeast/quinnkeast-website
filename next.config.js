e4a76be2bfc682ea35513f0b1e04b0b490284fc9
quinnkeast <hey@quinnkeast.com>
2023-02-06 10:50:30 +0100

    Implement slide row component as a temp fix

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
   webpack: (config, { isServer }) => {
     // Fixes npm packages that depend on `fs` module
     if (!isServer) {
-        config.resolve.fallback.fs = false;
+      config.resolve.fallback.fs = false;
     }
 
-    return config
-  }
-}
+    return config;
+  },
+};
