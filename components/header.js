6941831f02f3b7a7aae1dd967d1a562154eab70a
Quinn Keast <quinn@airplane.dev>
2023-06-30 06:45:15 -0500

    Fix responsiveness on menu (???)

--- components/header.js
+++ components/header.js
@@ -32,7 +32,7 @@ const menuItems = [
 ];
 
 const menuLink = (label, path) => (
-  <li key={path}>
+  <li key={path} className="mb-0 pb-0">
     <Link href={`/${path}`} className="border-0 hover:text-black">
       {label}
     </Link>
@@ -55,7 +55,7 @@ export default function Header() {
           <Link href="/" className="font-medium border-0 text-sm text-tan">
             Quinn Keast
           </Link>
-        <div className="col-2">
+        <div className="col-span-2">
           <menu className="flex flex-wrap gap-x-2 gap-y-0 list-none px-0 my-0 text-black-lighter text-sm text-tan leading-snug">
             {menuItems.map((item) => mobileMenuLink(item.label, item.path))}
           </menu>
@@ -63,13 +63,13 @@ export default function Header() {
       </div>
       <div className="container mx-auto px-4 xl:max-w-screen-xl">
         <div className="hidden md:grid md:grid-cols-3 md:mt-16 mb-4 md:mb-24">
-          <div className="col-1">
+          <div className="col-span-1">
               <Link href="/" className="font-medium border-0">
                 Quinn Keast
               </Link>
           </div>
-          <div className="col-2">
-            <menu className="grid grid-flow-col gap-10 list-none px-0 my-0 text-black-lighter">
+          <div className="col-span-2">
+            <menu className="flex flex-row md:flex-wrap md:gap-x-4 xl:gap-x-10 list-none px-0 my-0 text-black-lighter">
               {menuItems.map((item) => menuLink(item.label, item.path))}
             </menu>
           </div>
